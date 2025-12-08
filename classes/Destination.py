0fa79aaa07d92f2bcab9dd91110fc75dc1eb3753
Alex Hendrix <alex.hendrix@usu.edu>
2025-12-08 11:07:10 -0700

    fixed Destination3D to use a 3d position and removed unneeded z component

--- classes/Destination.py
+++ classes/Destination.py
@@ -13,10 +13,9 @@ class Destination2D:
 class Destination3D(Destination2D):
     def __init__(self,
                  position: Position3D,
-                 position_z: float,
                  target_speed_to_next_destination: int,
                  error: float,
                  ):
-        self.position = Position3D
+        self.position = position
         self.target_speed_to_next_destination = target_speed_to_next_destination
         self.error = error
