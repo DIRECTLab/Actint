8a9b78531ce120d7a7b491f17385b43463833f89
Alex Hendrix <alex.hendrix@usu.edu>
2025-12-08 11:27:06 -0700

    refactored destinations for inheritance

--- classes/Destination.py
+++ classes/Destination.py
@@ -1,21 +1,28 @@
-from Position import Position2D, Position3D
+from Position import Position, Position2D, Position3D
 
-class Destination2D:
+class Destination:
+    def __init__(self,
+                 position: Position,
+                 target_speed_to_next_destination: int,
+                 error: float,
+                 ):
+          self.position = position
+          self.target_speed_to_next_destination = target_speed_to_next_destination
+          self.error = error
+
+class Destination2D(Destination):
     def __init__(self,
                  position: Position2D,
                  target_speed_to_next_destination: int,
                  error: float,
                  ):
-        self.position = position
-        self.target_speed_to_next_destination = target_speed_to_next_destination
-        self.error = error
+        super().__init__(position, target_speed_to_next_destination, error)
+        
 
-class Destination3D(Destination2D):
+class Destination3D(Destination):
     def __init__(self,
                  position: Position3D,
                  target_speed_to_next_destination: int,
                  error: float,
                  ):
-        self.position = position
-        self.target_speed_to_next_destination = target_speed_to_next_destination
-        self.error = error
+        super().__init__(position, target_speed_to_next_destination, error)
