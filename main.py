ed12158868a23cae5656cbbe2e82246e7a4fdf07
Templeton <a02357165@usu.edu>
2026-01-14 11:26:14 -0700

    Finished integrating everything together. The main.py can now be run and it will use the a commandline filename argument as the runfile. It can be switched to using the simulation_data.csv file easily.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from csv_print import csv_print_data, csv_print_header
+from runfile import read_csv
+import sys
 
 def main():
     default_filename: str = "JFN-Groudtruth-Simulator_result.csv"
     filename: str = csv_print_header(default_filename)
+    vehicles:list = read_csv(sys.argv[1])  # Get filename from system arguments
+    # vehicles:list = read_csv("simulation_data.csv")  # Get filename from default arguments
+
 
     all_done=False
     while not all_done:
         """
-        iterate through a linked list of vehicle objects and call their update methods
+        iterate through a list of vehicle objects and call their update methods
         """
-        csv_print_data([], filename)
-        pass # Replace with actual simulation loop logic
+        for v in vehicles:
+            v.update(1, 10000, 10000)
+        csv_print_data(vehicles, filename)
+
+        all_done = all(v.action == 'done' for v in vehicles)
 
 
 
