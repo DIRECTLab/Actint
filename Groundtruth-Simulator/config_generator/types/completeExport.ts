27ffa03b6294e11dc98748ba52481d2796ef8512
Daxton Broadhead <daxton.broadhead21@gmail.com>
2026-02-10 15:17:08 -0700

    slightly less buggy config generator

--- Groundtruth-Simulator/config_generator/types/completeExport.ts
+++
@@ -1,7 +0,0 @@
-import { SimulationSettingsTyp } from "./simulationSettings"
-import { VehicleSettingsTyp } from './vehicleSettings'
-
-export type CompleteExport = {
-    sim_settings: SimulationSettingsTyp;
-    vehicles: [VehicleSettingsTyp]
-}
