a4da39db454a9df0be3f082ffe11b65fc8b84070
Daxton Broadhead <daxton.broadhead21@gmail.com>
2026-02-19 13:08:39 -0700

    rebasing stills

--- Groundtruth-Simulator/config-generator/server.py
+++ Groundtruth-Simulator/config-generator/server.py
@@ -12,12 +12,10 @@ def receive_points():
     data = request.json
     print(data)
 
-
     current_datetime = datetime.now()
     openString = "./output/simulation_settings_" + current_datetime.strftime("%Y-%m-%d_%H:%M:%S") + ".json"
 
     os.makedirs(os.path.dirname(openString), exist_ok=True)
-
 
     with open(openString, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
