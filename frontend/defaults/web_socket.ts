fe2c56f55918a01782efbfee58689fe7be049327
Alex Hendrix <alex.hendrix@usu.edu>
2026-05-06 10:36:21 -0600

    added quit functionality and fixed config issues

--- frontend/defaults/web_socket.ts
+++ frontend/defaults/web_socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const websocketUrl = process.env.NEXT_PUBLIC_WEBSOCKET_URL || "ws://129.123.61.22:3050";
+const websocketUrl = process.env.NEXT_PUBLIC_WEBSOCKET_URL || "ws://192.168.0.241:3050";
 export const socket = io(websocketUrl);
