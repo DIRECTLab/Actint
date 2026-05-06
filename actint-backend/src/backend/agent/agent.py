ec2df5bd45a106cafc769d2d8e23a990ba0a84f7
Nash Bryce <nashrbryce@gmail.com>
2026-05-06 16:43:16 +0000

    fixed misspelled var for config

--- actint-backend/src/backend/agent/agent.py
+++ actint-backend/src/backend/agent/agent.py
@@ -30,8 +30,8 @@ else:
 model_id = "Qwen/Qwen3.5-9B"
 #model_id = "Qwen/Qwen2-7B-Instruct"
 
-if config.conda_prefix:
-    python_path = str(Path(config.conda_prefix) / "bin" / "python")
+if config.CONDA_PREFIX:
+    python_path = str(Path(config.CONDA_PREFIX) / "bin" / "python")
 else:
     python_path = sys.executable
 
