b32ebb5e41f072d257d1441923f2ba028319ad9a
Alexander Hendrix <36391270+hendrixtothemax@users.noreply.github.com>
2026-06-03 13:47:08 -0600

    Alex/dockerbuild (#13)
    * Basic Docker Build Changes
    
    * connections working in docker between containers
    
    * added seperate start script for regular guff inference w/ llama.cpp
    
    * updated config to deal with full inference server URL replacement and partial like previously setup
    
    * tested builds without docker and they work
    
    * adding more instructions to the websocket url to make clear localhost should not be used by default
    
    * removed transformers requirement for building backend, added health check to the backend
    
    * cleaning up printing for checking llm connection
    
    * Minor change to draw_vessel_trajectory
    
    * Consolidating start inference scripts
    
    ---------
    
    Co-authored-by: Meikul <meikul.harris@gmail.com>

--- actint-backend/src/backend/agent/agent.py
+++ actint-backend/src/backend/agent/agent.py
@@ -30,6 +30,20 @@ else:
         file=sys.stderr
     )
 
+def check_openai_health(api_key="dummy") -> str:
+    url = f"{config.INFERENCE_SERVER_URL}/models"
+    headers = {"Authorization": f"Bearer {api_key}"}
+    
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+        if response.status_code == 200:
+            return "\033[1;32mAPI is operational and the connection is healthy and serving the following models:\033[0m " + "\n- ".join([model['id'] for model in response.json().get('data', [])])
+        else:
+            return f"\033[31mAPI returned error code: {response.status_code}\033[0m"
+    except requests.exceptions.RequestException as e:
+        return f"\033[31mNetwork/Connection failure: {str(e)}\033[0m"
+
+
 model_id = config.MODEL_ID or ""
 print("\033[0;34mModel ID: \033[1;34m" + model_id + "\033[0m")
 
@@ -56,6 +70,9 @@ model = TransformersModel(
 adsb_mcp_client = MCPClient(adsb_server_params, structured_output=False)
 adsb_mcp_tools = adsb_mcp_client.get_tools()
 
+
+print(f"""\033[1;33mChecking connection to llm inference server {config.INFERENCE_SERVER_URL}\033[0m""", file=sys.stderr)
+print(check_openai_health(), file=sys.stderr)
 
 model = OpenAIModel(
     model_id="local",
